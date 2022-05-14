4b3159477a378e238dd2d3d5362a56923d87d101
sebkk <sebastianmun11@gmail.com>
2022-05-14 12:26:34 +0200

    feat: implement login route

--- src/routes/base.js
+++ src/routes/base.js
@@ -3,11 +3,16 @@ import { lazy } from 'react'
 import { Loader } from '../components'
 
 const MainPage = Loader(lazy(() => import('../content/MainPage')))
+const Login = Loader(lazy(() => import('src/content/Auth/Login')))
 
 const baseRoutes = [
 	{
 		path: '/',
 		element: <MainPage />,
+	},
+	{
+		path: 'login',
+		element: <Login />,
 	},
 ]
 
